64d78a84cd0f0716cb664e387e4fed8134b617ce
AydosNB <nawrizbaevaydos50@gmail.com>
2024-08-19 20:45:33 +0500

    sdsds

--- src/components/pageComp/ModalAlert.jsx
+++ src/components/pageComp/ModalAlert.jsx
@@ -7,7 +7,7 @@ const ModalAlert = ({ children, title }) => {
     const dispatch = useDispatch()
     return (
         <div className='fixed z-20 top-0 left-0 right-0 bottom-0 bg-black backdrop-blur-[2px] bg-opacity-25 flex justify-center items-start pt-[10vh] pb-[20px] overflow-y-auto'>
-            <div className='p-[10px] rounded-md shadow-md bg-white w-[500px]'>
+            <div className='p-[10px] rounded-md shadow-md bg-white w-[500px] mx-[10px]'>
                 <div className="flex justify-between items-center">
                     <span className="text-[18px] font-semibold">{title}</span>
                     <button onClick={() => dispatch(toggleModalAlert())} className="p-[5px] rounded-md hover:bg-gray-100 active:scale-95">
